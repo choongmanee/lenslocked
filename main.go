ce176582630ec17e66e7bba57503831e39e91ca8
choongmanee <chung.man.kim@gmail.com>
2022-07-02 00:11:51 -0700

    middleware exercise

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
@@ -17,11 +18,26 @@ func contactHandler(writer http.ResponseWriter, request *http.Request) {
 func faqHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "<h1>faq</h1>")
 }
+
+func showHandler(writer http.ResponseWriter, request *http.Request) {
+	id := chi.URLParam(request, "id")
+	fmt.Fprint(writer, id)
+}
+
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Heartbeat("/ping"))
+	r.Route("/middleware", func(r chi.Router) {
+		r.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+			fmt.Fprint(writer, "<h1>middleware</h1>")
+		})
+	})
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Get("/show/{id}", showHandler)
+
 	r.NotFound(func(writer http.ResponseWriter, request *http.Request) {
 		http.Error(writer, "page not found", http.StatusNotFound)
 	})
